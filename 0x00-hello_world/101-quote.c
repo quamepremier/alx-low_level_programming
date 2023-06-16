176efa73ed79a913ea92b9898e381dd1d716be39
quamepremier <ericsarpong92@gmail.com>
2023-06-15 18:43:47 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,6 @@
 */
 int main(void)
 {
-	write(2,
-	"and that piece of art is useful\" - Dora Kopar, 2015-10-19\n", 59);
+	fput("and that piece of art is useful\" - Dora Kopar, 2015-10-19\n", stdout);
 	return (1);
 }
