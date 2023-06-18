adeb0649db037d90012284749e90a1885e35db04
quamepremier <ericsarpong92@gmail.com>
2023-06-17 17:53:28 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,10 +5,11 @@
 /**
  * main -  A C progrmme  that prints a line to the standard error
  * Return: Always 1 (Success)
- * */
+ */
 
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n
+	");
 	return (1);
 }
