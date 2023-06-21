1398666eac09596aaf55de1cec75f3d4748f1db2
quamepremier <ericsarpong92@gmail.com>
2023-06-21 14:36:03 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _alpha - Checks for the alphanumeric character
+ * @c: The character to be checked
+ * Return: 1 for alphanumeric character or 0 for anything else
+ */
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 98) || (c >= 97 && c <= 122))
+	return (1);
+	return (0);
+}
