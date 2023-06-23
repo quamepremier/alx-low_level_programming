b29005480e324a82c477d61a3b9567d797975c5c
quamepremier <ericsarpong92@gmail.com>
2023-06-22 17:56:07 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *more_numbers - Prints 10 times the numbers sine 0 up to 14
+ *more_numbers - Print 10 times the numbers since 0 up to 14
  *Return: 10 times of the numbers since 0 up to 14
  */
 
