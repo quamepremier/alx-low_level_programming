f41b3ebf7cd3cc4879f09dfb1fee38a673c7d873
quamepremier <ericsarpong92@gmail.com>
2023-06-26 19:56:39 -0700

    main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+
+
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -10,3 +17,5 @@ void puts_half(char *str);
 void print_array   (int *a, int n);
 char *_strecpy(char *dest, char *src);
 int _atoi(char *s);
+
+#endif
